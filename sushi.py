177cda3353aa617d04861b80199ec60905e071de
467011-hash <467011@wsk.ac.th>
2026-06-15 09:50:21 +0700

    Update dimensions input and volume calculation

--- sushi.py
+++ sushi.py
@@ -1,7 +1,7 @@
-width = 5
-height = 10
-lenght = 5
+width = int(input("กรอกความกว้าง"))
+height = int(input("กรอกความสูง"))
+lenght = int(input("กรอกความยาว"))
 space = width * height
-volume = space * lenght
+volume = height * width * lenght
 print("พื้นที่คือ" , space)
 print("ปริมาตรคือ" , volume)
