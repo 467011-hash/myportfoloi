5071fc733929f9bca3f895e197e931424b42663c
467011-hash <467011@wsk.ac.th>
2026-06-15 03:02:04 +0000

    first02.py

--- first02.py
+++ first02.py
@@ -1,1 +1,7 @@
-
+width = float(input("กรอกความกว้าง"))
+height = float(input("กรอกความสูง"))
+lenght = float(input("กรอกความยาว"))
+space = width * height
+volume = height * width * lenght
+print("พื้นที่คือ" , space)
+print("ปริมาตรคือ" , volume)
